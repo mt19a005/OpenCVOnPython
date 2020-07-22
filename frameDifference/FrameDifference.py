26316f571e86032bd681319d6c97a66a044c31c0
Motoharu Taguwa <mt19a005@oecu.jp>
2020-07-22 23:42:01 +0900

    フレーム差分法完成

--- frameDifference/FrameDifference.py
+++ frameDifference/FrameDifference.py
@@ -1,7 +1,9 @@
 #-*- coding:utf-8 -*-
 import cv2
-import numpy 
+import numpy as np
 import time
+
+frame = 0.5
 
 cap = cv2.VideoCapture(0)
 
@@ -18,20 +20,36 @@ while(cap.isOpened()):
     img1 = cap.read()[1]
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
-    # チャンネルが1（グレースケール）だったら
-    if img3[2] == 1:
-        cv2.absdiff(img2, img1,)
+    # チャンネルが無し（グレースケール）だったら
+    try:
+        img3.shape[2]
+    except IndexError:
+        # 絶対値差分　これによって、2画像の動きがわかる
+        diff1 = cv2.absdiff(img2, img1)
+        diff1 =  cv2.threshold(diff1, 10, 255,cv2.THRESH_BINARY)[1]
+        diff2 = cv2.absdiff(img2, img3)
+        diff2 =  cv2.threshold(diff2, 10, 255,cv2.THRESH_BINARY)[1]
+        
+        # 光の点滅を差分にしないためにオープニングをする
+        kernel = np.ones((3, 3), np.uint8)
+        diff1 = cv2.morphologyEx(diff1, cv2.MORPH_OPEN, kernel)
+        
+        # 2画像の白が重なっている部分だけを抽出
+        diffbitwiseAnd = cv2.bitwise_and(diff1, diff2)
 
-    cv2.imshow("imgNew", img1)
-    cv2.imshow("imgCurrent", img2)
-    cv2.imshow("imgOld", img3)
-    time.sleep(1)
+        # 膨張・収縮処理(方法2)
+        cv2.imshow("diff1", diff1)
+        cv2.imshow("diff2", diff2)
+        cv2.imshow("diffbitwiseAnd", diffbitwiseAnd)
+
+
+    cv2.imshow("img1", img1)
+    cv2.imshow("img2", img2)
+    cv2.imshow("img3", img3)
+    time.sleep(frame)
     # xキーが押されたら途中終了
     if cv2.waitKey(1) & 0xFF == ord('x'):
         break
-    time.sleep
-
-
 
 cap.release()
 cv2.destroyAllWindows()
